3833f70a4ebd60642dfcd6c94ab93150e5743ba1
Julio Lopez <julioj.lopeza@gmail.com>
2018-10-28 22:52:11 -0300

    Adding Name to Sign up form, and adding styles to sign up and sign in forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+ 
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation) }
+    end
+
 end
