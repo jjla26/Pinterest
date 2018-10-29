5287305ede5ed94f095dbd1c4d664accb230e5e7
Julio Lopez <julioj.lopeza@gmail.com>
2018-10-28 23:06:11 -0300

    Fixing registration form issue with new feature(name)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 class ApplicationController < ActionController::Base
-
     before_action :configure_permitted_parameters, if: :devise_controller?
- 
-    protected
 
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation) }
-    end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+  end
 
 end
