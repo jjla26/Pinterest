ff0ec8bda3b8a03ccc3c6d8d4ed808a297f2b5b2
Julio Lopez <julioj.lopeza@gmail.com>
2018-11-05 00:54:32 -0300

    New View (pinsof)

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -20,9 +20,16 @@ class PinsController < ApplicationController
     @pins = current_user.pins
   end
 
+  def pinsof
+    @user_id = params[:user_id]
+    @user = User.find(@user_id)
+    @pins = @user.pins
+  end
+
   # GET /pins/1
   # GET /pins/1.json
   def show
+
   end
 
   # GET /pins/new
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'products/index'
   get 'mypins'  => 'pins#mypins'
+  get 'pinsof/:user_id'  => 'pins#pinsof', :as => 'pinsof'
   resources :pins
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
